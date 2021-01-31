8bb700439dabce9102e665b21369786daa65fb5f
felipegxavier <felipexavier20015@gmail.com>
2021-01-31 12:48:18 -0300

    Add user migration and docker config

--- token/nodejs/server.js
+++ token/nodejs/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => res.send('Ok'));
+app.get("/", (req, res) => res.send("Ok"));
 
 app.listen(3000);
