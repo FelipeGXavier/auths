0af0882f8ed5bb2497c7edc03a94985acc3ce8a0
felipegxavier <felipexavier20015@gmail.com>
2021-02-07 13:07:04 -0300

    Add apidoc config to nodejs application

--- api-key/nodejs/server.js
+++ api-key/nodejs/server.js
@@ -1,3 +1,7 @@
+const express = require('express');
 const app = require('./src/api');
+const path = require('path');
+
+app.use('/apidoc', express.static(path.join(__dirname, 'apidoc')));
 
 app.listen(3000);
--- api-key/nodejs/src/api/index.js
+++ api-key/nodejs/src/api/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const app = express();
 
 const Datasource = require('../data/datasource');
@@ -8,13 +7,10 @@ const datasource = new Datasource();
 const GeneralRoutes = require('../routes/generalRoutes');
 const generalRoutes = new GeneralRoutes(datasource).routes();
 
-
 const GuardRoutes = require('../routes/guardRoutes');
 const guardRoutes = new GuardRoutes(datasource).routes();
 
 app.use(express.json());
-
-
 app.use('/api/datasource', generalRoutes);
 app.use('/api/guard', guardRoutes);
 
