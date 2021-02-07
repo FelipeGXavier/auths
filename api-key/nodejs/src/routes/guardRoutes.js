91fed66ca8d6d662a1fa509e2ff33e8e0da76738
felipegxavier <felipexavier20015@gmail.com>
2021-02-07 16:01:35 -0300

    Add doc to guard routes

--- api-key/nodejs/src/routes/guardRoutes.js
+++ api-key/nodejs/src/routes/guardRoutes.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
 class GuardRoutes {
-    
   constructor(datasource) {
     this.datasource = datasource;
   }
 
   routes() {
+    /**
+     * @api {get} /api/guard Get value from key in header
+     * @apiGroup Guard
+     *
+     * @apiHeader {String} x-api-key Key present in datasource
+     *
+     * @apiErrorExample Error-Response:
+     * Error 403: Forbidden
+     * Forbidden
+     */
     router.get("/", this.filter(this.datasource), (req, res) => {
       res.send(req.value);
     });
